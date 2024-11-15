3ca224a4f447a4ad73ea14aa5ecba12cbf92b37c
Vladislav <hhhhhhhhgggggggggggfffffffffff@yandex.by>
2024-11-15 12:57:14 +0300

    search

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
-var page = location;
+// var page = location;
 
-console.log(page)
+// console.log(page)
+
+
+
+var inputGamesSearch = document.querySelector('#findGame');
+
+
+inputGamesSearch.oninput = function(){
+	let gamesList = document.querySelectorAll('li');
+	let inputData = inputGamesSearch.value.trim().toLowerCase();
+	if(inputData != ''){
+		gamesList.forEach(function(el){
+			let search = el.innerText.toLowerCase().search(inputData);
+
+			if(search == -1){
+				el.style.display = "none";
+				el.innerHTML = el.innerText;
+			}else{
+				el.style.display = "";
+				el.innerHTML = gamesListSearch(el.innerText, search, inputData.length);
+				
+				// gamesListSearch(el.innerText, search, inputData.length);
+				// console.log(inputData.length)
+				
+			}
+		});
+	}else{
+		gamesList.forEach(function(el){
+			el.style.display = '';
+			el.innerHTML = el.innerText;
+		})
+	}
+}
+
+function gamesListSearch(str, pos, len){
+	console.clear();
+	console.log(str, pos, len)
+	// console.log(str.slice(0, pos), str.slice(pos, pos+len), str.slice(pos+len))
+	return str.slice(0, pos) + '<b>' + str.slice(pos, pos+len) + '</b>' + str.slice(pos+len);
+
+}
