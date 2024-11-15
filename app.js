b3b59d8b383814b87fdcf144a7e21ea9fe80be18
Vladislav <hhhhhhhhgggggggggggfffffffffff@yandex.by>
2024-11-15 13:31:49 +0300

    upd search

--- app.js
+++ app.js
@@ -10,34 +10,39 @@ var inputGamesSearch = document.querySelector('#findGame');
 inputGamesSearch.oninput = function(){
 	let gamesList = document.querySelectorAll('li');
 	let inputData = inputGamesSearch.value.trim().toLowerCase();
+	let block = [];
+
 	if(inputData != ''){
 		gamesList.forEach(function(el){
 			let search = el.innerText.toLowerCase().search(inputData);
 
 			if(search == -1){
+
 				el.style.display = "none";
 				el.innerHTML = el.innerText;
+				el.parentElement.parentElement.style.display = 'none';
 			}else{
+				let blockId = el.parentElement.parentElement.attributes.index.value;
+				block.push(blockId);
 				el.style.display = "";
 				el.innerHTML = gamesListSearch(el.innerText, search, inputData.length);
-				
-				// gamesListSearch(el.innerText, search, inputData.length);
-				// console.log(inputData.length)
-				
 			}
 		});
+		block.forEach(function(id){
+			let el = document.querySelector(`[index="${id}"]`);
+			el.style.display = '';
+		})
 	}else{
+		block = [];
 		gamesList.forEach(function(el){
 			el.style.display = '';
 			el.innerHTML = el.innerText;
+			el.parentElement.parentElement.style.display = '';
 		})
 	}
 }
 
 function gamesListSearch(str, pos, len){
-	console.clear();
-	console.log(str, pos, len)
-	// console.log(str.slice(0, pos), str.slice(pos, pos+len), str.slice(pos+len))
 	return str.slice(0, pos) + '<b>' + str.slice(pos, pos+len) + '</b>' + str.slice(pos+len);
 
 }
