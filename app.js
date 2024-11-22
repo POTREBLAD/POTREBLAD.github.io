5d2c97390775955f7b985582d4cdd17b2e5de04e
Vladislav <hhhhhhhhgggggggggggfffffffffff@yandex.by>
2024-11-22 20:40:51 +0300

    steam h

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// var page = location;
-
-// console.log(page)
 
 
 
 var inputGamesSearch = document.querySelector('#findGame');
+var inputAddGame = document.querySelector('#addGame');
 
 let GamePosters = document.querySelectorAll('img');
 
@@ -14,27 +12,33 @@ GamePosters.forEach(function(el){
 	let gamename = el.title;
 	let gameheader = bg.querySelector('.game-header');
 	el.onclick = function(){
-		if(el.classList.value.indexOf('img-poster-big') != -1){
-			el.classList.remove('img-poster-big');
-			el.style.left = 0;
-			bg.style.opacity = 0;
-			// el.style.left = "";
-			// bg.style.zIndex = "-1";
-			bg.style.display = "";
-			el.parentElement.style.height = '';
-			
-		}else{
-			gameheader.innerText = gamename;
-			el.classList.add('img-poster-big');
-			bg.style.display = "block";
-			bg.style.zIndex = 1;
-			el.parentElement.style.height = '300px';
-			el.style.left = "";
-			bg.style.opacity = 1;
-			// el.style.left = 5+'px';
-			el.style.left = -el.offsetLeft + 8 + 5 + 'px';
-		}
+		if(el.parentElement.attributes.index.value != 11){
+
 		
+			if(el.classList.value.indexOf('img-poster-big') != -1){
+				el.classList.remove('img-poster-big');
+				el.style.left = 0;
+				bg.style.opacity = 0;
+				// el.style.left = "";
+				// bg.style.zIndex = "-1";
+				bg.style.display = "";
+				el.parentElement.style.height = '';
+				
+			}else{
+				gameheader.innerText = gamename;
+				el.classList.add('img-poster-big');
+				// alert();
+				bg.style.display = "block";
+				bg.querySelector('.playtime').innerText = Math.floor(el.attributes.playtime.value / 60);
+
+				bg.style.zIndex = 1;
+				el.parentElement.style.height = '300px';
+				el.style.left = "";
+				bg.style.opacity = 1;
+				// el.style.left = 5+'px';
+				el.style.left = -el.offsetLeft + 8 + 5 + 'px';
+			}
+		}
 	}
 });
 
@@ -67,6 +71,7 @@ inputGamesSearch.oninput = function(){
 			el.style.display = '';
 		})
 	}else{
+		document.querySelector(`[index="10"]`).style.display = "";
 		block = [];
 		gamesList.forEach(function(el){
 			el.style.display = '';
@@ -89,3 +94,57 @@ function gamesListSearch(str, pos, len){
 	return str.slice(0, pos) + '<b>' + str.slice(pos, pos+len) + '</b>' + str.slice(pos+len);
 
 }
+
+inputAddGame.oninput = function(){
+	let title = inputAddGame.value;
+	let addGameList = document.getElementById('addGameList');
+	axios.post('/getGame', {
+		title: title, 
+	})
+	.then(function (response) {
+		let data = response.data;
+		addGameList.innerText = '';
+		
+		for(let el in data){
+			let str = data[el]['title'];
+			let id = data[el]['id'];
+			let steamid = data[el]['steamid'];
+			let pos = str.toLowerCase().search(title.toLowerCase());
+			let len = title.length;
+			// console.log(pos, len)
+			// console.log(data[el]);
+			addGameList.innerHTML += `<li >${gamesListSearch(str, pos, len)} <a onclick="addGameInList('', ${steamid}, ${id})" href=#>Добавить</a></li>`;
+			
+		}
+		// console.log(response);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+	console.log(inputAddGame.value);
+}
+
+function remove(id, el){
+	axios.post('/remove', {
+		id: id
+	})
+	.then(function (response) {
+		el.remove();
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+}
+
+document.onkeydown = function(e){
+	if(e.keyCode == 17) {
+		// alert('ss');
+		document.onclick = function(e){
+			// console.log(e);
+			remove(e.target.attributes.index.value, e.target);
+			// e.target.remove();
+			// console.log()
+		}
+	} 
+}
+
